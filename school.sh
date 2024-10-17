8b9d36769552074aa50049ac469db5ab2432274e
Shuo Li <sli2254@wisc.edu>
2024-10-17 14:43:48 -0500

    Modified school.sh to output both sum and average

--- school.sh
+++ school.sh
@@ -4,4 +4,13 @@ cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" |
 
 cut -d',' -f7 |
 
-awk '{sum += $1; count += 1} END {if (count > 0) print "Average TotalAssessedValue: " sum / count; else print "No data found"}'
+awk '{sum += $1; count += 1} 
+     END {
+         if (count > 0) {
+             print "Sum: " sum;
+             print "Average: " sum / count;
+         } else {
+             print "No data found."
+         }
+     }'
+# awk '{sum += $1; count += 1} END {if (count > 0) print "Average TotalAssessedValue: " sum / count; else print "No data found"}'
