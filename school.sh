451f19f159f44fb4e98148a3440dd66c55f44d14
Shuo Li <sli2254@wisc.edu>
2024-10-17 14:31:47 -0500

    Added script to calculate average TotalAssessedValue

---
+++ school.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" |
+
+cut -d',' -f7 |
+
+awk '{sum += $1; count += 1} END {if (count > 0) print "Average TotalAssessedValue: " sum / count; else print "No data found"}'
